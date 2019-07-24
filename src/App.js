6b1120eeb43d0e0e855369c12550e4c5e5015f2b
DONADEI Emrick <emrick.donadei@hotmail.fr>
2019-07-25 00:08:15 +0200

    adding the handle values of the form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
 import ExpenseForm from "./components/ExpenseForm";
@@ -24,24 +24,48 @@ const initialExpenses = [
 ];
 
 function App() {
-  // 1: The json with expenses 2: The function
+  // ******* state values *******
+  // 1: The json with expenses 2: The function for adding expense
   const [expenses, setExpenses] = useState(initialExpenses);
-  console.log(expenses);
-  console.log(setExpenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+
+  // ******* functionnality *******
+  const handleCharge = e => {
+    setCharge(e.target.value);
+  };
+
+  const handleAmount = e => {
+    setAmount(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
 
   return (
     <React.Fragment>
       <Alert />
       <h1>Budget calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
-        total spending : <span className="total">
-          € {expenses.reduce((accumulator,current) => {
+        total spending :{" "}
+        <span className="total">
+          €{" "}
+          {expenses.reduce((accumulator, current) => {
             return (accumulator += current.amount);
-          },0)}
+          }, 0)}
         </span>
       </h1>
     </React.Fragment>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,24 +1,44 @@
 import React from "react";
 import { MdSend } from "react-icons/md";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({
+  charge,
+  amount,
+  handleCharge,
+  handleAmount,
+  handleSubmit
+}) => {
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="form-center">
         <div className="form-group">
-          <label htmlFor="amount">charge</label>
+          <label htmlFor="charge">charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g. rent"
+            value={charge}
+            onChange={handleCharge}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
           <input
             type="text"
             className="form-control"
             id="amount"
             name="amount"
             placeholder="e.g. 100"
+            value={amount}
+            onChange={handleAmount}
           />
         </div>
       </div>
       <button type="submit" className="btn">
-          submit 
-          <MdSend className="btn-icon"/>
+        submit
+        <MdSend className="btn-icon" />
       </button>
     </form>
   );
