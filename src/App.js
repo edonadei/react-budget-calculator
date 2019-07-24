f918965de9af3d3bb463694a2c6f5b8ed2324881
DONADEI Emrick <emrick.donadei@hotmail.fr>
2019-07-24 21:38:51 +0200

    init code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="">
+      <h2>
+        Hello from app
+      </h2>
+    </div>
+  );
+}
+
+export default App;
